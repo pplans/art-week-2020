9aaea0d6ae5379cd0353c1c0fedba40815373922
David Gond <david.gond.coding@gmail.com>
2020-04-25 18:54:02 +0200

    refactor(character): Integrated player controls into Player script, improved score management, refactored members visibility

--- art-week-2020/Assets/Scripts/Characters/Player.cs
+++ art-week-2020/Assets/Scripts/Characters/Player.cs
@@ -7,13 +7,28 @@ namespace Assets.Scripts.Characters
     {
         #region Members
 
+        public int Score
+        {
+            get;
+            private set;
+        }
+
+        [SerializeField]
+        private readonly float _rotationSpeed = 100.0F;
+
+        private float _horizontal;
+        private float _vertical;
+
+        private Rigidbody _rigidbody;
+        private readonly Vector3 _eulerAngleVelocity = new Vector3(0, 100, 0);
+
         #endregion
 
         #region UnityEvents
         public override void Awake()
         {
             base.Awake(); // Call parent init
-
+            _rigidbody = GetComponent<Rigidbody>();
         }
 
         public void Start()
@@ -27,8 +42,30 @@ namespace Assets.Scripts.Characters
         public override void UpdateCharacter()
         {
             base.UpdateCharacter(); // Call parent update
-            if (Input.GetKeyDown("space"))
-                EffectManager.AddSpeedBuff(0.1f, this, isInfinite: true);
+            
+            ManageInput();
+        }
+
+        public void AddScore(int amount)
+        {
+            Score += amount;
+        }
+
+        private void ManageInput()
+        {
+            _horizontal = Input.GetAxis("Horizontal");
+            _vertical = Input.GetAxis("Vertical");
+
+            if (_vertical != 0)
+            {
+                _rigidbody.MovePosition(transform.position + transform.forward * Speed * _vertical * Time.deltaTime);
+            }
+
+            if (_horizontal != 0)
+            {
+                Quaternion deltaRotation = Quaternion.Euler(_eulerAngleVelocity * _horizontal * Time.deltaTime * _rotationSpeed);
+                _rigidbody.MoveRotation(_rigidbody.rotation * deltaRotation);
+            }
         }
         #endregion
     }
---
+++ art-week-2020/Assets/Scripts/UI/ScoreText.cs
@@ -0,0 +1,21 @@
+﻿using Assets.Scripts.Characters;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.UI
+{
+    public class ScoreText : MonoBehaviour
+    {
+        public Player Player;
+        private Text _score;
+
+        // Start is called before the first frame update
+        private void Start()
+        {
+            _score = GetComponent<Text>();
+        }
+
+        // Update is called once per frame
+        private void Update() => _score.text = "Score: "+ Player.Score;
+    }
+}
